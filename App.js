5a6dc3a1141bc756ad4409566c0fad3575f00aa8
mlmartin2 <martinm@usp.br>
2022-04-08 20:41:21 -0300

    segunda tentativa sqlite nao ta dando certoooooooooooooooo

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import LoginScreen from './screens/LoginScreen';
 import SignUpScreen from './screens/SignupScreen';
 import HomeScreen from './screens/HomeScreen';
-//import * as SQLite from 'expo-sqlite';
 import SQLite from 'react-native-sqlite-storage'
 
 // const db = SQLite.openDatabase(
@@ -23,14 +22,14 @@ import SQLite from 'react-native-sqlite-storage'
 //   })
 // }
 
-async function getConnection()
-{
-  const db = SQLite.openDatabase('elobase.db')
-  db.transaction(db.executeSql("CREATE TABLE IF NOT EXISTS"
-  +"Users"))
-  //alert(db)
-  return db
-}
+// async function getConnection()
+// {
+//   const db = SQLite.openDatabase('elobase.db')
+//   db.transaction(db.executeSql("CREATE TABLE IF NOT EXISTS"
+//   +"Users"))
+//   //alert(db)
+//   return db
+// }
 
 // SQLite.enablePromise(true)
 
@@ -40,29 +39,116 @@ async function getConnection()
 //   (error) => {alert(error)}
 // )
 
+
+
+SQLite.enablePromise(true)
+
+const db = SQLite.openDatabase({name:'elodb'})
+.then((datab) => {alert(datab)})
+
 export default function App() {
 
   const StackNavigator = createNativeStackNavigator();
   const [database, setDatabase] = useState()
-  
+  const [dbInit, setDbInit] = useState(false)
+
+  //alert(promise)
 
   useEffect(() =>
   {
-    setDatabase(getConnection())
-    //getConnection()
+
+    //db.then(() => {alert(db)}, (error) => {alert(error)} )
+    // db.transaction((tx) => tx.executeSql("CREATE TABLE IF NOT EXISTS"
+    // +"Users", (error) => alert(error.message + " Error on creating"), () => alert('done'))) 
+    // setDatabase(db)
+    // getConnection()
+    // setDbInit(true)
+    // try
+    // {
+    //   SQLite.openDatabase({name: 'elobase', location:'default'},() => alert('top'), (e) => alert(e.text))
+    // }
+    // catch(error)
+    // {
+    //   alert(error.text)
+    // }
+    //dBase()
+    createTables()
   }, [])
 
-  const CreateTable = () =>
+  const createTables = () => 
   {
-    if(database != undefined)
+    try
     {
-      alert('db exists')
-    }    
+      alert(database)
+      // database.then((tx) => tx.transaction((tx) => {
+      //   tx.executeSql('CREATE TABLE IF NOT EXISTS'+'User')
+      // }),(error) => alert(error + 'error load'))
+    }
+    catch(error)
+    {
+      alert(error.message)
+    }
   }
+  // async function TryDB()
+  // {
+  //   try
+  //   {
+  //     results = await Database.excuteSql("SQL",['params']);
+  //   }
+  //   catch(e)
+  //   {
+   
+  //   }
+  // }
 
-  CreateTable()
+  // useEffect(() => 
+  // {
+  //   if(dbInit)
+  //   {
+  //     try
+  //     {
+  //       database.transaction((tx) =>
+  //       {
+  //         tx.executeSql("CREATE TABLE IF NOT EXISTS " + "Users")
+  //       })
+  //     }
+  //     catch(error) {alert(error.message)}
+  //   }
+  // }, [dbInit])
 
-  alert(database)
+  console.log('a')
+
+  // const ReadTable = () =>
+  // {
+  //   if(database != undefined)
+  //   {
+  //     alert(database + 'connection')
+  //     try{database.transaction((tx) => {
+  //       tx.executeSql('SELECT * FROM Users', [], (tx) => alert('a'), (tx, err) => alert(err.message + "read error") )
+  //     })}
+  //     catch(error){ { alert(error + "@ ReadTable()")}} 
+  //   }  
+  // }
+
+  // const CreateTable = () =>
+  // {
+  //   try
+  //   {
+  //     alert('creating table')
+  //     database.transaction((tx) =>
+  //     {
+  //       tx.executeSql('CREATE TABLE IF NOT EXISTS'
+  //       +'User')
+  //     })
+  //   }
+  //   catch(error)
+  //   {
+  //     alert(error + "@ CreatTable()")
+  //   }
+  // }
+
+  //try {CreateTable()}
+  //catch(error) {alert(error)}
 
   return (
     <NavigationContainer>
